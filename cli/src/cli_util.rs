8fce1968a0499059e888b9abea06e9cd6ca61921
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2025-03-08 16:29:24 +0000

    Start diffedit with boxes checked
    Co-authored-by: Valentin Gatien-Baron <valentin.gatienbaron@gmail.com>

--- cli/src/cli_util.rs
+++ cli/src/cli_util.rs
@@ -181,6 +181,7 @@ use crate::formatter::FormatRecorder;
 use crate::formatter::Formatter;
 use crate::formatter::FormatterExt as _;
 use crate::merge_tools::DiffEditor;
+use crate::merge_tools::InitialSelection;
 use crate::merge_tools::MergeEditor;
 use crate::merge_tools::MergeToolConfigError;
 use crate::operation_templater::OperationTemplateLanguage;
@@ -3500,6 +3501,7 @@ impl DiffSelector {
                             Diff::new(trees.before, &selected_tree),
                             matcher,
                             format_instructions,
+                            InitialSelection::None,
                         )
                         .await?)
                 }
